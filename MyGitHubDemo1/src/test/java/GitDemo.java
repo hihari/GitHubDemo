3c0c14a6944a33ef5073e2c7153dd08d81d303a1
xz279d <xz279d@A6145221.nw.nos.boeing.com>
2019-05-16 06:51:18 -0400

    Change for 3 Syso

--- MyGitHubDemo1/src/test/java/GitDemo.java
+++ MyGitHubDemo1/src/test/java/GitDemo.java
@@ -4,6 +4,8 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git Hub");
+		System.out.println("Hello Git Hub");
+		System.out.println("Hello Git Hub");
 	}
 
 }
